05d0dcb8d5d082fb170c852a15e1b8d2f2fe2bb7
lakitha-dias <lakitha.dias@gmail.com>
2020-10-17 22:32:18 +0530

    31. Return Custom Error Message Object

--- src/main/java/com/appsdeveloperblog/app/ws/exceptions/AppExceptionHandler.java
+++ src/main/java/com/appsdeveloperblog/app/ws/exceptions/AppExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.appsdeveloperblog.app.ws.exceptions;
 
 
+import java.util.Date;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.appsdeveloperblog.app.ws.ui.model.response.ErrorMessage;
+
 
 // centralized place that can handle our exceptions
 @ControllerAdvice
@@ -16,6 +20,13 @@ public class AppExceptionHandler extends ResponseEntityExceptionHandler{
 	
 	@ExceptionHandler(value = {Exception.class})
 	public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest request){
-		return new ResponseEntity<>(ex,new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		String errorMessageDescription = ex.getLocalizedMessage();
+		
+		if(errorMessageDescription == null) errorMessageDescription = ex.toString();
+		
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), errorMessageDescription);
+		
+		return new ResponseEntity<>(errorMessage,new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
