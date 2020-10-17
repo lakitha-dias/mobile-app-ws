458e512e0e11b5bca2cfc4021fba43a5c0ad04ad
lakitha-dias <lakitha.dias@gmail.com>
2020-10-17 22:53:58 +0530

    34. Catch More Than One Exception with One Method

--- src/main/java/com/appsdeveloperblog/app/ws/exceptions/AppExceptionHandler.java
+++ src/main/java/com/appsdeveloperblog/app/ws/exceptions/AppExceptionHandler.java
@@ -1,8 +1,6 @@
 package com.appsdeveloperblog.app.ws.exceptions;
 
-
 import java.util.Date;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,8 +28,8 @@ public class AppExceptionHandler extends ResponseEntityExceptionHandler{
 		return new ResponseEntity<>(errorMessage,new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
-	@ExceptionHandler(value = {NullPointerException.class})
-	public ResponseEntity<Object> handleNullPointerException(NullPointerException ex, WebRequest request){
+	@ExceptionHandler(value = {NullPointerException.class,UserServiceException.class})
+	public ResponseEntity<Object> handleSpecificException(Exception ex, WebRequest request){
 		
 		String errorMessageDescription = ex.getLocalizedMessage();
 		
@@ -42,15 +40,19 @@ public class AppExceptionHandler extends ResponseEntityExceptionHandler{
 		return new ResponseEntity<>(errorMessage,new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
-	@ExceptionHandler(value = {UserServiceException.class})
-	public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request){
-		
-		String errorMessageDescription = ex.getLocalizedMessage();
-		
-		if(errorMessageDescription == null) errorMessageDescription = ex.toString();
-		
-		ErrorMessage errorMessage = new ErrorMessage(new Date(), errorMessageDescription);
-		
-		return new ResponseEntity<>(errorMessage,new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
-	}
+	/*
+	 * @ExceptionHandler(value = {UserServiceException.class}) public
+	 * ResponseEntity<Object> handleUserServiceException(UserServiceException ex,
+	 * WebRequest request){
+	 * 
+	 * String errorMessageDescription = ex.getLocalizedMessage();
+	 * 
+	 * if(errorMessageDescription == null) errorMessageDescription = ex.toString();
+	 * 
+	 * ErrorMessage errorMessage = new ErrorMessage(new Date(),
+	 * errorMessageDescription);
+	 * 
+	 * return new ResponseEntity<>(errorMessage,new HttpHeaders(),
+	 * HttpStatus.INTERNAL_SERVER_ERROR); }
+	 */
 }
